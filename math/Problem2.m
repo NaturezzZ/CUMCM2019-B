5e0c56e28c35d882db3c3825af8614c49e280f59
NaturezzZ <naturezzz@outlook.com>
2019-09-14 15:45:01 +0800

    Prob2,3

--- math/Problem2.m
+++ math/Problem2.m
@@ -1,7 +1,6 @@
-% 主要思想是，在鼓上升的过程中，减小鼓的倾斜角
 % 以对称轴为y轴，则各个力位于(4i/pi + bias)的位置
 F = [80,80,80,80,80,80,80,80];
-activate = [0,1,0,0,0,0,0,0];
+activate = [0,0,0,0,0,1,0,0];
 bias = 0;
 theta = [];
 I = 0.0865;
@@ -17,27 +16,15 @@ for i = 1:10000
     for j = 1:8
         r = [0.2*sin(theta(1,j))*cos(delta), 0.2*cos(theta(1,j))*cos(delta),0.2*sin(delta)];
         Fj = [F(1,j)*sin(theta(1,j))*cos(alpha), F(1,j)*cos(theta(1,j))*cos(alpha), F(1,j)*sin(alpha)];
-        % r
-        % Fj
         if activate(1,j)==1
             M = M + cross(r,Fj);   
         end
     end
-    %M
-    % break;
     delta = delta + omega* 0.1/10000;
     omega = omega + M(1,2)/I * 0.1/10000;
 end
-delta
-
-for i = 1:8
-    if activate(1,i) ==1
-        F(1,i) = 0;
-    end
-end
-
 %second period
-for i = 1:40000
+for i = 1:(30000)
     M = [0,0,0];
     for j = 1:8
         r = [0.2*sin(theta(1,j))*cos(delta), 0.2*cos(theta(1,j))*cos(delta),0.2*sin(delta)];
@@ -46,11 +33,8 @@ for i = 1:40000
         % Fj
         M = M + cross(r,Fj); 
     end
-    %M
     delta = delta + omega* 0.1/10000;
     omega = omega + M(1,2)/I * 0.1/10000;
-    %M
-    %omega
-    %break;
 end
+omega
 delta
---
+++ math/Problem3.m
@@ -0,0 +1,89 @@
+% 主要思想是，在鼓上升的过程中，减小鼓的倾斜角
+% 以对称轴为y轴，则各个力位于(4i/pi + bias)的位置
+F = [80,80,80,80,80,80,80,80];
+activate = [0,0,0,0,0,0,0,0];
+bias = 0;
+theta = [];
+I = 0.0865;
+for i = 1:8
+    theta(1,i) = pi*i/4 +bias;
+end
+alpha = 0.11/1.7;
+delta = 0;
+omega = 0;
+% period 1
+for i = 1:10000
+    M = [0,0,0];
+    for j = 1:8
+        r = [0.2*sin(theta(1,j))*cos(delta), 0.2*cos(theta(1,j))*cos(delta),0.2*sin(delta)];
+        Fj = [F(1,j)*sin(theta(1,j))*cos(alpha), F(1,j)*cos(theta(1,j))*cos(alpha), F(1,j)*sin(alpha)];
+        % r
+        % Fj
+        if activate(1,j)==1
+            M = M + cross(r,Fj);   
+        end
+    end
+    delta = delta + omega* 0.1/10000;
+    omega = omega + M(1,2)/I * 0.1/10000;
+end
+"period 1"
+["omega","delta";omega,delta]
+% period 2
+for i = 1:(10000)
+    M = [0,0,0];
+    for j = 1:8
+        r = [0.2*sin(theta(1,j))*cos(delta), 0.2*cos(theta(1,j))*cos(delta),0.2*sin(delta)];
+        Fj = [F(1,j)*sin(theta(1,j))*cos(alpha), F(1,j)*cos(theta(1,j))*cos(alpha), F(1,j)*sin(alpha)];
+        % r
+        % Fj
+        M = M + cross(r,Fj); 
+    end
+    %M
+    % ;
+    delta = delta + omega* 0.1/10000;
+    omega = omega + M(1,2)/I * 0.1/10000;
+    
+    %omega
+    %break;
+end
+"period 2"
+["omega","delta";omega,delta]
+
+% he noticed he was early and dimished his force
+F_backup = F;
+for i = 1:8
+    if activate(1,i) ==1
+        F(1,i) = 1.5*F(1,i);
+    end
+end
+% period 3
+for i = 1:(10000)
+    M = [0,0,0];
+    for j = 1:8
+        r = [0.2*sin(theta(1,j))*cos(delta), 0.2*cos(theta(1,j))*cos(delta),0.2*sin(delta)];
+        Fj = [F(1,j)*sin(theta(1,j))*cos(alpha), F(1,j)*cos(theta(1,j))*cos(alpha), F(1,j)*sin(alpha)];
+        M = M + cross(r,Fj); 
+        %r
+        %Fj
+        %cross(r,Fj)
+    end
+    delta = delta + omega* 0.1/10000;
+    omega = omega + M(1,2)/I * 0.1/10000;
+end
+"period 3"
+["omega","delta";omega,delta]
+
+% period 4
+F = F_backup;
+for i = 1:(20000)
+    M = [0,0,0];
+    for j = 1:8
+        r = [0.2*sin(theta(1,j))*cos(delta), 0.2*cos(theta(1,j))*cos(delta),0.2*sin(delta)];
+        Fj = [F(1,j)*sin(theta(1,j))*cos(alpha), F(1,j)*cos(theta(1,j))*cos(alpha), F(1,j)*sin(alpha)];
+        M = M + cross(r,Fj); 
+    end
+    delta = delta + omega* 0.1/10000;
+    omega = omega + M(1,2)/I * 0.1/10000;
+end
+"period 4"
+["omega","delta";omega,delta]
